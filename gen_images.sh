48450b671cfa73ccfe759d049bf2576f20160d93
Andrew Rae <ajrae.nv@gmail.com>
2022-01-16 18:57:48 -0500

    Fixing typo in gen_images

--- gen_images.sh
+++ gen_images.sh
@@ -4,5 +4,5 @@ container_cmd=docker
 container_args="-w /board -v $(pwd):/board --rm"
 
 ${container_cmd} run ${container_args} yaqwsx/kikit:v1.0.0 pcbdraw --style builtin:oshpark-afterdark.json output/pcbs/board-routed.kicad_pcb images/board-front.png
-${container_cmd} run ${container_args} yaqwsx/kikit:v1.0.0 pcbdraw -b --style builtin:oshpark-afterdark.json output/pcbs/board-routed.kicad_pcb images/board-right.png
+${container_cmd} run ${container_args} yaqwsx/kikit:v1.0.0 pcbdraw -b --style builtin:oshpark-afterdark.json output/pcbs/board-routed.kicad_pcb images/board-back.png
 # ${container_cmd} run ${container_args} yaqwsx/kikit:v0.7 kikit fab jlcpcb --no-assembly output/pcbs/board-routed.kicad_pcb production/pcb/board
